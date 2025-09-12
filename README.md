cc3a81c05f79fb25f3ec73163fe4b5150c6a8b3f
jc <juan.cruz.business@gmail.com>
2025-09-12 07:32:58 -0500

    add

---
+++ README.md
@@ -0,0 +1,20 @@
+# rustlings
+
+
+```
+# Call this to enter container shell
+apptainer run \
+--bind ./:/app \
+./.apptainer/rust.sif
+```
+
+```
+# Installation
+cargo install rustlings
+# Initialization
+rustlings init
+# Moving into new directory
+cd rustlings
+# Starting Rustlings
+rustlings
+```
---
+++ rustlings/exercises/quizzes/quiz2.rs
@@ -0,0 +1,95 @@
+// This is a quiz for the following sections:
+// - Strings
+// - Vecs
+// - Move semantics
+// - Modules
+// - Enums
+//
+// Let's build a little machine in the form of a function. As input, we're going
+// to give a list of strings and commands. These commands determine what action
+// is going to be applied to the string. It can either be:
+// - Uppercase the string
+// - Trim the string
+// - Append "bar" to the string a specified amount of times
+//
+// The exact form of this will be:
+// - The input is going to be a Vector of 2-length tuples,
+//   the first element is the string, the second one is the command.
+// - The output element is going to be a vector of strings.
+
+enum Command {
+    Uppercase,
+    Trim,
+    Append(usize),
+}
+
+mod my_module {
+    use super::Command;
+
+    // TODO: Complete the function as described above.
+    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String>{
+
+        let mut results: Vec<String> = vec![];
+
+        for item in input{
+
+            let mut temp_str: String = item.0;
+
+
+            match item.1{
+                Command::Uppercase => {
+                    temp_str.make_ascii_uppercase();
+                }
+                Command::Trim => {
+                    temp_str = temp_str.trim_ascii().to_string();
+                }
+                Command::Append(num) => {
+                    for _ in 0..num {
+                        temp_str.push_str("bar");
+                    }
+
+                }
+            }
+
+            results.push(temp_str);
+        }
+
+
+        results
+
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    // TODO: What do we need to import to have `transformer` in scope?
+    // use ???;
+    use super::my_module::transformer as transformer;
+    use super::Command;
+
+    #[test]
+    fn it_works() {
+        let input = vec![
+            ("hello".to_string(), Command::Uppercase),
+            (" all roads lead to rome! ".to_string(), Command::Trim),
+            ("foo".to_string(), Command::Append(1)),
+            ("bar".to_string(), Command::Append(5)),
+        ];
+        let output = transformer(input);
+
+        assert_eq!(
+            output,
+            [
+                "HELLO",
+                "all roads lead to rome!",
+                "foobar",
+                "barbarbarbarbarbar",
+            ]
+        );
+    }
+}
